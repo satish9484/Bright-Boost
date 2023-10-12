afebf2d6e61238aa3b0ce1fd02fa9af10ec8a514
litvinova08 <ma.litvinova08@gmail.com>
2023-10-12 12:18:16 +1100

    form completed; student can submit an inqury to the Admin

--- src/pages/Helpline/index.js
+++ src/pages/Helpline/index.js
@@ -2,18 +2,12 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { db } from "../../firebase/firebase";
 import "./style.scss";
-
-// import { db } from "../../../firebase/firebase";
 import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import moment from "moment";
-import { Button, DatePicker, Form, Select, ConfigProvider } from "antd";
+import { Button, Form, Select, notification } from "antd";
 import BreadCrumbs from "../../components/common/Breadcrumbs";
 import Card from "../../components/common/Card";
-
-import enUS from "antd/lib/locale/en_US";
-
 const { Option } = Select;
-const dateFormatList = "DD/MM/YYYY";
 
 const subjects = [
 	"English",
@@ -35,37 +29,36 @@ const collectionName = "Bright-Boost";
 const documentId = "helpline";
 
 const initialStudentData = {
-	email: "", // Add logic to populate this with the user's email
-	subject: "English",
-	tutor: tutorsBySubject["English"][0] || "",
+	email: "",
+	subject: "",
+	tutor: "",
 	date: "",
 	time: "",
-	sessionTime: 1, // Set a default session time here
+	inquiry: "",
 };
 
 const Helpline = () => {
 	const { currentUser } = useContext(AuthContext);
 	const [currentTime] = useState(moment().format("HH:mm:ss"));
-	const [lastDayOfYear] = useState(moment().endOf("year"));
+	const [currentDate] = useState(moment().format());
+	const [inquryMessage, setInquryMessage] = useState("");
 	const [form] = Form.useForm();
-	const [selectedDate, setSelectedDate] = useState(null);
+	const [tutorListForSubject, setTutorListForSubject] = useState(
+		tutorsBySubject[initialStudentData.subject] || []
+	);
 
 	// Function to handle form submission
 	const handleSubmit = async (values) => {
 		try {
-			const selectedMoment = moment(values?.date);
-			setSelectedDate(selectedMoment.format(dateFormatList));
-
 			const docRef = doc(db, collectionName, documentId);
 			const docSnapshot = await getDoc(docRef);
-
 			const dataToStore = {
 				email: currentUser?.email || "",
 				subject: values.subject,
-				date: selectedDate,
+				date: currentDate,
 				time: currentTime,
-				// sessionTime: values.sessionTime,
 				tutor: values.tutor,
+				inquiry: inquryMessage,
 			};
 
 			if (docSnapshot.exists()) {
@@ -81,47 +74,42 @@ const Helpline = () => {
 				await setDoc(docRef, dataToSet);
 			}
 
-			console.log("Student record added successfully!");
+			console.log("Student inquiry has been sent to Admin successfully!");
+			showSuccessNotification(); // display a notification
+			form.resetFields(); // Reset the form fields
+			setInquryMessage(""); // Reset the inquiry message state
 		} catch (error) {
-			console.error("Error adding student record: ", error);
+			console.error("Error sending student inqury: ", error);
 		}
 	};
 
-	// Function to disable dates in the past and future
-	const disabledDate = (current) => {
-		return (
-			(current && current < moment().startOf("day")) ||
-			(current && current > lastDayOfYear.endOf("day"))
-		);
+	//Set student inqury message
+	const handleInquiryChange = (e) => {
+		setInquryMessage(e.target.value);
 	};
 
-	// Function to handle date change
-	const handleDateChange = (date, dateString) => {
-		setSelectedDate(dateString);
+	//display a message once student inqury has been submitted
+	const showSuccessNotification = () => {
+		notification.success({
+			message: "Success",
+			description: "Your inquiry has been successfully submitted!",
+		});
 	};
 
 	// Function to handle subject selection
-	// const handleSubjectChange = (value) => {
-	// 	if (value in tutorsBySubject) {
-	// 		setTutorListForSubject(tutorsBySubject[value]);
-	// 		form.setFieldsValue({ tutor: tutorsBySubject[value][0] || "" });
-	// 	} else {
-	// 		console.log("false value");
-	// 		setTutorListForSubject([]);
-	// 	}
-	// };
+	const handleSubjectChange = (value) => {
+		if (value in tutorsBySubject) {
+			setTutorListForSubject(tutorsBySubject[value]);
+			form.setFieldsValue({ tutor: tutorsBySubject[value][0] || "" });
+		} else {
+			console.log("false value");
+			setTutorListForSubject([]);
+		}
+	};
 
 	return (
 		<>
-			<BreadCrumbs
-			// list={[
-			// 	{
-			// 		name: "Session Registration",
-			// 		link: "/student/sessionregistration",
-			// 		isActive: false,
-			// 	},
-			// ]}
-			/>
+			<BreadCrumbs />
 			<Card>
 				<Form
 					form={form}
@@ -150,7 +138,7 @@ const Helpline = () => {
 						>
 							<Select
 								placeholder="Select Subject"
-								// onChange={handleSubjectChange}
+								onChange={handleSubjectChange}
 							>
 								{subjects.map((subject) => (
 									<Option key={subject} value={subject}>
@@ -167,36 +155,18 @@ const Helpline = () => {
 							name="tutor"
 							rules={[
 								{
-									// required: true,
 									message: "Please select a tutor",
 								},
 							]}
 						>
 							<Select placeholder="Tutor Name">
-								{/* {tutorListForSubject.map((tutor) => (
+								{tutorListForSubject.map((tutor) => (
 									<Option key={tutor} value={tutor}>
 										{tutor}
 									</Option>
-								))} */}
+								))}
 							</Select>
 						</Form.Item>
-
-						{/* <Form.Item
-							className="col-xl-6 col-md-8 form-group"
-							label="Session Duration"
-							labelWrap={true}
-							name="sessionTime"
-						> */}
-						{/* <Select placeholder="Select Subject"> */}
-						{/* {sessionTime.map((sessionTime) => (
-									<Option key={sessionTime} value={sessionTime}>
-										{sessionTime} hour
-									</Option>
-								))} */}
-						{/* </Select> */}
-						{/* </Form.Item> */}
-
-						{/* <ConfigProvider locale={enUS}> */}
 						<div className="inquiry">
 							<Form.Item
 								className="col-xl-6 col-md-8 form-group"
@@ -206,19 +176,17 @@ const Helpline = () => {
 								rules={[
 									{
 										required: true,
-										message: "Please select a date",
+										message: "Please enter your inquiry",
 									},
 								]}
 							>
-								<textarea className="inquiry-text"></textarea>
-								{/* <DatePicker
-									format={dateFormatList}
-									disabledDate={disabledDate}
-									onChange={handleDateChange}
-								/> */}
+								<textarea
+									className="inquiry-text"
+									value={inquryMessage} // Set the value of the textarea
+									onChange={handleInquiryChange} // Update inquiryMessage on change
+								></textarea>
 							</Form.Item>
 						</div>
-						{/* </ConfigProvider> */}
 
 						<div className="col-xl-12 d-flex justify-content-end mar-top-8">
 							<Button className="mar-right-8">Cancel</Button>
